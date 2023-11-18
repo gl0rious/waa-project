b34bec7e03dc1d578f3c248afa15733da5d2c085
gl0rious <majidgues@gmail.com>
2023-11-18 15:25:45 -0600

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductPage from "./components/products-page/ProductList.jsx";
 import ShoppingCartPage from "./components/shopping-cart/ShoppingCartPage";
 import AddProductForm from "./pages/product/AddProductForm.jsx";
@@ -7,29 +7,28 @@ import ProductDetail from "./components/products-page/ProductDetail.jsx";
 import Checkout from "./pages/checkout-page/Checkout.jsx";
 import OrderTable from "./pages/OrderTable.jsx";
 import UpdateProductForm from "./pages/product/UpdateProductForm.jsx";
-import ShoppingCartApp from "./pages/ShoppingCart.jsx";
-import ShoppingCartItems from "./pages/ShoppingCart.jsx";
-import ShoppingCart from "./pages/ShoppingCart.jsx";
+// import ShoppingCartApp from "./pages/ShoppingCart.jsx";
+// import ShoppingCartItems from "./pages/ShoppingCart.jsx";
+// import ShoppingCart from "./pages/ShoppingCart.jsx";
 
 function App() {
-
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/products" element={<ProductPage/>}/>
-                    <Route path="/products" element={<ProductPage/>}/>
-                    <Route path="/shoppingcart" element={<ShoppingCartPage/>}/>
-                    <Route path="/addproduct" element={<AddProductForm/>}/>
-                    <Route path="/products/:productId" element={<ProductDetail/>}/>
-                    <Route path="/checkout" element={<Checkout/>}/>
-                    <Route path="/orders" element={<OrderTable/>}/>
-                    <Route path="/updateproduct" element={<UpdateProductForm/>}/>
-                    <Route path="/shop" element={<ShoppingCart/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/products" element={<ProductPage />} /> */}
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/shoppingcart" element={<ShoppingCartPage />} />
+          <Route path="/addproduct" element={<AddProductForm />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<OrderTable />} />
+          <Route path="/updateproduct" element={<UpdateProductForm />} />
+          {/* <Route path="/shop" element={<ShoppingCart/>}/> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
